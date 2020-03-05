402e03de02358b5ce6cf9bfaf22c9dfd58cebcf4
NTTData <tahmeed.ahmed@nttdata.com>
2020-03-05 12:57:27 +0000

    Finished task

---
+++ src/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+
+    public int sum2(int a, int b) { return a+b; }
+    public int subtract2(int a, int b) { return a-b; }
+    public int divide2(int a, int b) { return a/b; }
+    public int multiply2(int a, int b) { return a*b; }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.sum2(1,2));
+        System.out.println(cal.subtract2(8,4));
+        System.out.println(cal.multiply2(10,10));
+        System.out.println(cal.divide2(10,5));
+    }
+
+
+}
